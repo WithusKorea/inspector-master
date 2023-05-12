0058dffed990de8b1b1634ddfcdf63e5b50ad31c
Johnny <developer5601@gmail.com>
2023-05-13 07:52:48 +0900

    menuStrip1 컨트롤 추가

--- Inspector/Forms/MainForm.Designer.cs
+++ Inspector/Forms/MainForm.Designer.cs
@@ -31,7 +31,11 @@
             this.panel_Container = new System.Windows.Forms.Panel();
             this.panel_Viewer = new System.Windows.Forms.Panel();
             this.panel_SystemMessage = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.프로그램ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel_Container.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_Container
@@ -39,9 +43,9 @@
             this.panel_Container.Controls.Add(this.panel_Viewer);
             this.panel_Container.Controls.Add(this.panel_SystemMessage);
             this.panel_Container.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel_Container.Location = new System.Drawing.Point(0, 0);
+            this.panel_Container.Location = new System.Drawing.Point(0, 24);
             this.panel_Container.Name = "panel_Container";
-            this.panel_Container.Size = new System.Drawing.Size(1086, 615);
+            this.panel_Container.Size = new System.Drawing.Size(1086, 591);
             this.panel_Container.TabIndex = 0;
             // 
             // panel_Viewer
@@ -49,16 +53,40 @@
             this.panel_Viewer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel_Viewer.Location = new System.Drawing.Point(0, 0);
             this.panel_Viewer.Name = "panel_Viewer";
-            this.panel_Viewer.Size = new System.Drawing.Size(1086, 501);
+            this.panel_Viewer.Size = new System.Drawing.Size(1086, 477);
             this.panel_Viewer.TabIndex = 0;
             // 
             // panel_SystemMessage
             // 
             this.panel_SystemMessage.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel_SystemMessage.Location = new System.Drawing.Point(0, 501);
+            this.panel_SystemMessage.Location = new System.Drawing.Point(0, 477);
             this.panel_SystemMessage.Name = "panel_SystemMessage";
             this.panel_SystemMessage.Size = new System.Drawing.Size(1086, 114);
             this.panel_SystemMessage.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.프로그램ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1086, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 프로그램ToolStripMenuItem
+            // 
+            this.프로그램ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.종료ToolStripMenuItem});
+            this.프로그램ToolStripMenuItem.Name = "프로그램ToolStripMenuItem";
+            this.프로그램ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.프로그램ToolStripMenuItem.Text = "프로그램";
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
             // 
             // MainForm
             // 
@@ -66,12 +94,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1086, 615);
             this.Controls.Add(this.panel_Container);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.panel_Container.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,5 +113,8 @@
         private System.Windows.Forms.Panel panel_Container;
         private System.Windows.Forms.Panel panel_Viewer;
         private System.Windows.Forms.Panel panel_SystemMessage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 프로그램ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
     }
 }
